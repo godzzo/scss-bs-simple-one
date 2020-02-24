91a0bfafe29bc33792545e6a83d93efaaebee1b4
godzzolp <godzzo@lightport.hu>
2020-02-24 18:22:15 +0100

    webpack-dev-server added with watch 🙂
    the --hot option important, sourcemap working too 😁

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,11 @@ module.exports = {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	},
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 9000
+	},
 	module: {
 		rules: [{
 			test: /\.(scss)$/,
